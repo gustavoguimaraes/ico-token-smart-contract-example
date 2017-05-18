ee95cc80c1d641abe8573a26e484344c7d1a1fd5
Gustavo Guimaraes <gustavoguimaraes@gmail.com>
2017-05-18 10:32:52 +0200

    ICO and toke contract demo with Truffle and Open Zeppelin"

---
+++ README.md
@@ -0,0 +1,2 @@
+# Create Token and ICO Contracts using Truffle and Open zeppelin
+Demo app as referred in [this blog post](https://medium.com/@gus_tavo_guim/how-to-create-token-and-initial-coin-offering-contracts-using-truffle-openzeppelin-1b7a5dae99b6)
---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,11 @@
+var CrowdsaleToken = artifacts.require("./zeppelin/token/CrowdsaleToken.sol")
+
+module.exports = function(deployer) {
+  deployer.deploy(CrowdsaleToken);
+};
+
+var CrowdsaleToken = artifacts.require("./zeppelin/token/CrowdsaleToken.sol")
+
+module.exports = function(deployer) {
+  deployer.deploy(CrowdsaleToken);
+};
